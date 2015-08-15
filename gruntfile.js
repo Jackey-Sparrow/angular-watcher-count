18040872f59fb7f68f9f71fc89d86f08ad8472fa
Jackey <jackey.li@foxmail.com>
2015-08-15 11:16:02 +0800

    use gulp to run test case

---
+++ gruntfile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Jackey Li on 2015/8/15.
+ */
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+
+    grunt.registerTask('default', []);
+
+};
--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     clean = require('gulp-clean'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    karma = require('gulp-karma');
 
 gulp.task('build-min', function () {
     return gulp.src('src/*.js')
@@ -18,9 +19,20 @@ gulp.task('build', function () {
         .pipe(gulp.dest('build'));
 });
 
+gulp.task('runTest',function(){
+    return gulp.src('test/watcher-spec.js')
+        .pipe(karma({
+            configFile:'test/karma.conf.js',
+            action:'run'
+        }))
+        .on('error', function (err) {
+            throw err;
+        });
+});
+
 gulp.task('clean', function () {
     return gulp.src(['build/*.js'], {read: false})
         .pipe(clean({force: true}));
 });
 
-gulp.task('default', ['clean', 'build','build-min']);
+gulp.task('default', ['clean', 'build','build-min','runTest']);
